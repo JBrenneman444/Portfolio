aa24ccd74654d81f023c2920f7e83234934de7ad
LocalPlacesWith <jbrenneman444@gmail.com>
2020-06-02 22:27:40 -0700

    enabled DOM-changing behavior on mouse hover

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
 $( () => {
-    //jQuery code goes here
+    
+  $("#aboutButton").on({ // select Project Button
+    
+    "mouseover" : function() { // on Mouse Over, do the following
+      var image = $("#main-image") // set local variable for main image
+      $(this).css("background-color","darkred") // set background for the Mouse Over'd button to GREEN
+      image.css("background-image",'url("https://images.twinkl.co.uk/tr/image/upload/illustation/Sad-Face-1.png")') // change bg img for main img
+    },
+    "mouseout" : function() { // undo everything
+      var image = $("#main-image")
+      $(this).css("background-color","black")
+      image.css("background-image",'url("https://images-na.ssl-images-amazon.com/images/I/51zLZbEVSTL._AC_SY355_.jpg")')
+    }
+
+  });
+
+  $("#projectsButton").on({ // select Project Button
+    
+    "mouseover" : function() { // on Mouse Over, do the following
+      var image = $("#main-image") // set local variable for main image
+      $(this).css("background-color","darkgreen") // set background for the Mouse Over'd button to GREEN
+      image.css("background-image",'url("https://images.twinkl.co.uk/tr/image/upload/illustation/Sad-Face-1.png")') // change bg img for main img
+    },
+    "mouseout" : function() { // undo everything
+      var image = $("#main-image")
+      $(this).css("background-color","black")
+      image.css("background-image",'url("https://images-na.ssl-images-amazon.com/images/I/51zLZbEVSTL._AC_SY355_.jpg")')
+    }
+    
+  });
+
+  $("#resumeButton").on({ // select Project Button
+    
+    "mouseover" : function() { // on Mouse Over, do the following
+      var image = $("#main-image") // set local variable for main image
+      $(this).css("background-color","darkblue") // set background for the Mouse Over'd button to GREEN
+      image.css("background-image",'url("https://images.twinkl.co.uk/tr/image/upload/illustation/Sad-Face-1.png")') // change bg img for main img
+    },
+    "mouseout" : function() { // undo everything
+      var image = $("#main-image")
+      $(this).css("background-color","black")
+      image.css("background-image",'url("https://images-na.ssl-images-amazon.com/images/I/51zLZbEVSTL._AC_SY355_.jpg")')
+    }
+    
+  });
+  
+
   });
