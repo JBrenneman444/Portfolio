2dc4cbcfe24ec44a7a552b1bba27a7f75102f113
LocalPlacesWith <jbrenneman444@gmail.com>
2020-06-04 17:08:33 -0700

    close to enabling appropriate  hover + click functionality

--- app.js
+++ app.js
@@ -16,26 +16,29 @@ $( () => {
   $("#aboutButton").on({ // select ABOUT Button
     
     "mouseover" : function() { // on Mouse Over, do the following
-      $(this).css("background-color","darkred") // set background for the Mouse Over'd button to GREEN
-      $("#main-image").css("background-image",'url("https://i.pinimg.com/originals/6a/2f/67/6a2f67c1823fe82035f53db68fe27666.png")') // change bg img for main img
-      $("#aboutContent").show()
+
+      if (selectedContent === 'about' || 'default') {
+        console.log('selectedContent is ABOUT or DEFAULT')
+        $(this).css("background-color","darkred") // set background for the Mouse Over'd button to GREEN
+        $("#main-image").css("background-image",'url("https://i.pinimg.com/originals/6a/2f/67/6a2f67c1823fe82035f53db68fe27666.png")') // change bg img for main img
+        $("#aboutContent").show()
+      } else {
+        console.log('selectedContent is NOT ABOUT or DEFAULT')
+      }
+
+      // $(this).css("background-color","darkred") // set background for the Mouse Over'd button to GREEN
+      // $("#main-image").css("background-image",'url("https://i.pinimg.com/originals/6a/2f/67/6a2f67c1823fe82035f53db68fe27666.png")') // change bg img for main img
+      // $("#aboutContent").show()
     },
     "mouseout" : function() { // undo everything
       $(this).css("background-color",defaultButtonColor)
       $("#main-image").css("background-image",'url('+defaultImage+')')
 
-      if (selectedContent === 'default') {
+      if (selectedContent === 'about') {
+        // do nothing
+      } else {
         $("#aboutContent").hide()
         $('#displayedContent').css('overflow-y','hidden')
-
-      } else if (selectedContent === 'about') {
-
-      } else if (selectedContent === 'projects') {
-
-      } else if (selectedContent === 'resume') {
-
-      } else {
-        // do nothing
       }
 
     },
@@ -45,16 +48,18 @@ $( () => {
       // $('#displayedContent').animate({scrollTop: $(window).scrollTop() + relativeScroll}, "slow")
       // $('#displayedContent').scrollTop($(window).scrollTop() + relativeScroll) // -- INSTANT
 
-      // TODO: make selectedContent/selectedImage the SELECTED content/image
+    // TODO: make selectedContent/selectedImage the SELECTED content/image
       
-      // TODO: LEAVE information UP
-      // $("#aboutContent").css("display","block")
+    // TODO: make CLICKED content STAY VISIBLE, even if MOUSE OFF
+      $("#projectsContent").hide()
+      $("#resumeContent").hide()
 
-      // $("p").html(function(n){
-      //   return "This p element has index: " + n;
-      // });
-      // selectedContent = $('#aboutContent')
-      // selectedImage = $
+      if (selectedContent === 'about') {
+        selectedContent = 'default'
+      } else {
+        selectedContent = 'about'
+      }
+    
     }
       
   });
@@ -62,20 +67,41 @@ $( () => {
   $("#projectsButton").on({ // select PROJECT Button
     
     "mouseover" : function() { // on Mouse Over, do the following
-      $(this).css("background-color","darkgreen") // set background for the Mouse Over'd button to GREEN
-      $("#main-image").css("background-image",'url("https://images.emojiterra.com/google/android-pie/512px/1f914.png")') // change bg img for main img
-      $("#projectsContent").show()
+      
+      if (selectedContent === 'projects' || 'default') {
+        console.log('selectedContent is PROJECTS or DEFAULT')
+        $(this).css("background-color","darkgreen") // set background for the Mouse Over'd button to GREEN
+        $("#main-image").css("background-image",'url("https://images.emojiterra.com/google/android-pie/512px/1f914.png")') // change bg img for main img
+        $("#projectsContent").show()
+      } else {
+        console.log('selectedContent is NOT PROJECTS or DEFAULT')
+      }
     },
     "mouseout" : function() { // undo everything
       $(this).css("background-color",defaultButtonColor)
       $("#main-image").css("background-image",'url('+defaultImage+')')
-      $("#projectsContent").hide()
+
+      if (selectedContent === 'projects') {
+        // do nothing
+      } else {
+        $("#projectsContent").hide()
+        $('#displayedContent').css('overflow-y','hidden')
+      }
+
     },
     "click" : function() { // IF user CLICKS, go to ACTUAL CONTENT
       $('#displayedContent').css('overflow-y','auto')
       $('#displayedContent').animate({scrollTop: $(window).scrollTop() + relativeScroll}, 1200)
-      // $('#displayedContent').animate({scrollTop: $(window).scrollTop() + relativeScroll}, "slow")
-      // $('#displayedContent').scrollTop($(window).scrollTop() + relativeScroll) // -- INSTANT
+      
+      $("#aboutContent").hide()
+      $("#resumeContent").hide()
+
+      if (selectedContent === 'projects') {
+        selectedContent = 'default'
+      } else {
+        selectedContent = 'projects'
+      }
+
     }
     
   });
@@ -83,20 +109,40 @@ $( () => {
   $("#resumeButton").on({ // select RESUME Button
     
     "mouseover" : function() { // on Mouse Over, do the following
-      $(this).css("background-color","darkblue") // set background for the Mouse Over'd button to GREEN
-      $("#main-image").css("background-image",'url("https://i.pinimg.com/originals/b4/e0/9a/b4e09ae68a2a2ae9fc3cee5349364e3a.jpg")') // change bg img for main img
-      $("#resumeContent").show()
+
+
+      if (selectedContent === 'resume' || 'default') {
+        console.log('selectedContent is RESUME or DEFAULT')
+        $(this).css("background-color","darkblue") // set background for the Mouse Over'd button to GREEN
+        $("#main-image").css("background-image",'url("https://i.pinimg.com/originals/b4/e0/9a/b4e09ae68a2a2ae9fc3cee5349364e3a.jpg")') // change bg img for main img
+        $("#resumeContent").show()
+      } else {
+        console.log('selectedContent is NOT RESUME or DEFAULT')
+      }
     },
     "mouseout" : function() { // undo everything
       $(this).css("background-color",defaultButtonColor)
       $("#main-image").css("background-image",'url('+defaultImage+')')
-      $("#resumeContent").hide()
+      
+      if (selectedContent === 'resume') {
+        // do nothing
+      } else {
+        $("#resumeContent").hide()
+        $('#displayedContent').css('overflow-y','hidden')
+      }
     },
     "click" : function() { // IF user CLICKS, go to ACTUAL CONTENT
       $('#displayedContent').css('overflow-y','auto')
       $('#displayedContent').animate({scrollTop: $(window).scrollTop() + relativeScroll}, 1200)
-      // $('#displayedContent').animate({scrollTop: $(window).scrollTop() + relativeScroll}, "slow")
-      // $('#displayedContent').scrollTop($(window).scrollTop() + relativeScroll) // -- INSTANT
+      
+      $("#aboutContent").hide()
+      $("#projectsContent").hide()
+      if (selectedContent === 'resume') {
+        selectedContent = 'default'
+      } else {
+        selectedContent = 'resume'
+      }
+
     }
     
   });
